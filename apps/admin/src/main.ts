4245f2557ae093a29f54938f7400bf66bddb4062
hzp <zhoupingh@outlook.com>
2024-05-29 16:41:39 +0800

    feat(assets): create assets package reset styles

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -2,11 +2,12 @@ import { createApp } from 'vue'
 import { setupRouter } from '@naive-pro/router'
 import { setupStore } from '@naive-pro/store'
 import { setupI18n } from '@naive-pro/i18n'
+import { setupAssets } from '@naive-pro/assets'
 import App from './App.vue'
 
-import 'virtual:uno.css'
-
 async function bootstrap() {
+  setupAssets()
+
   const app = createApp(App)
 
   setupStore(app)
